83f2ebb5b0e97f78155b433e5c3c1b3e398a7ae2
Ahnaf Tahmid <tahmidtahmid31@gmail.com>
2024-03-12 16:38:32 +0600

    controlbar, todoview barebone

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import './App.css';
+import "./App.css";
+
+const mockData = [
+  {
+    id: "f1fd83e818a8838138",
+    title: "Note 3",
+    description: "Note descrition",
+    priority: 2,
+    status: "pending",
+    createdAt: "3/12/2024, 11:20:23 AM",
+    updatedAt: "4/12/2024, 11:20:23 AM",
+  },
+  {
+    id: "d2fd83e811v8838138",
+    title: "Note 3",
+    description: "Note descrition",
+    priority: 2,
+    status: "pending",
+    createdAt: "3/12/2024, 11:20:23 AM",
+    updatedAt: "4/12/2024, 11:20:23 AM",
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
     <>
-      <h1>Hi what should I do??</h1>
+      <div className="wrapper">
+        <ControlBar />
+        <TodosView todos={todos} />
+      </div>
     </>
   );
 }
---
+++ src/components/Controlbar.jsx
@@ -0,0 +1,12 @@
+const ControlBar = ({}) => {
+  return (
+    <div>
+      <button>add</button>
+      <button>trash</button>
+      <button>theme</button>
+      <button>shortcuts</button>
+    </div>
+  );
+};
+
+export default ControlBar;
