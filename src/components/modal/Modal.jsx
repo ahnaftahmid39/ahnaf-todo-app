7ed407d6cb09a94e8d24a28e4a3dae6e66ac7bf1
Ahnaf Tahmid <tahmidtahmid31@gmail.com>
2024-03-14 17:45:06 +0600

    added modal functionality

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,7 +1,18 @@
+import { createPortal } from "react-dom";
 import styles from "./Modal.module.scss";
 
-const Modal = ({ children }) => {
-  return <div>{children}</div>;
+const Modal = ({ open, handleClose, children, ...props }) => {
+  if (!open) return null;
+  return createPortal(
+    <>
+      <div className={styles["overlay"]} onClick={handleClose}></div>
+      <div className={styles["modal"]} {...props}>
+        {children}
+        <button onClick={handleClose}>Close</button>
+      </div>
+    </>,
+    document.getElementById("portal")
+  );
 };
 
 export default Modal;
