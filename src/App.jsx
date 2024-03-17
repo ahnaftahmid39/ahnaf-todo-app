49d4f904a3e74d0312b6ed9f1a7a28f3cabced20
Ahnaf Tahmid <tahmidtahmid31@gmail.com>
2024-03-17 13:02:22 +0600

    placed control bar at top

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,8 @@ function App() {
   return (
     <>
       <div className={`wrapper theme-${theme}`}>
-        <FiltersAndHeadingsBar />
         <ControlBar />
+        <FiltersAndHeadingsBar />
         <TodosView />
       </div>
     </>
